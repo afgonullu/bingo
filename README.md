0f6116df2d281a81d2ab9a595b40558bbfcc1217
Abdullah Faruk Gönüllü <contact@afaruk.dev>
2022-01-30 09:40:30 +0300

    initial representation

--- README.md
+++ README.md
@@ -8,9 +8,12 @@
 - [ x ] tailwind
 - [ x ] updating knowledge on aformentioned packages
   distributed 1 hrs
-- [ ] mockData
-- [ ] mockFetch
-- [ ] initial representation
+- [ x ] mockData
+- [ x ] mockFetch
+  distributed 20-30 minutes
+- [ x ] initial representation
+- [ x ] add more data set and select box
+  30 mins
 - [ ] logic
 - [ ] improve styling
 - [ ] backend + db
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react"
+
+import { data } from "./mockData"
+
 const Home: React.FC = () => {
+  const [selection, setSelection] = useState("books and authors worth reading")
+  const [bingoData, setBingoData] = useState<string[]>([])
+
+  useEffect(() => {
+    const array = [...data[selection]].sort(() => Math.random() - 0.5)
+    array.splice(12, 0, "FREE BINGO")
+    setBingoData(array)
+  }, [selection])
+
+  const handleSelect = (e: any) => {
+    setSelection(e.target.value)
+  }
+
   return (
     <div className="h-96">
-      <h1>Home</h1>
+      <select value={selection} onChange={handleSelect}>
+        {Object.keys(data).map((key) => (
+          <option key={key} value={key}>
+            {key}
+          </option>
+        ))}
+      </select>
+      <div className="bingoCardContainer">
+        {bingoData.map((item, index) => {
+          return (
+            <div key={index} className="bingoCard">
+              {item}
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
